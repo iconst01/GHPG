fa3d669a02464b55d2c7336ac259fb07b3dd328a
Itaty <itatyconstante@gmail.com>
2024-10-10 15:59:24 -0400

    add files again

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('Gbtn').addEventListener('click', function() {
+    var query=  document.getElementById ('search').value;
+     alert ('Search Query: ' + query);
+   });
+   
